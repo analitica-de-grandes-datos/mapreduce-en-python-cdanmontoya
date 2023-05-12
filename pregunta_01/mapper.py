082f5d48e5ff72067b1cda36e77a157a4c46bb9e
cdanmontoya <carlos.danmontoya@gmail.com>
2023-05-12 17:59:08 -0500

    Pregunta 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import csv
+
+with open('credit.csv', 'r') as file:
+    content = csv.reader(file)
+
+    for row in content:
+        mapped_value = row[2].rstrip()
+        print(f'{mapped_value}')
