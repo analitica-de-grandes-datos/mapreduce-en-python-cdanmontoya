0d9d997816bebe276d4a42aca09a1931608949a2
cdanmontoya <carlos.danmontoya@gmail.com>
2023-05-12 19:37:30 -0500

    Pregunta 7

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -3,8 +3,18 @@
 #
 import sys
 
-temp = {}
+temp = []
 
 for line in sys.stdin:
+    if line != '\n':
     # Combine
-    pass
+        char, date, number = line.split('   ')
+        mapped_tuple = (char, date, number.rstrip())
+        temp.append(mapped_tuple)
+
+# Reduce
+temp = sorted(temp, key=lambda x: (x[0], int(x[2])))
+
+# Format
+for item in temp:
+    print(f'{str(item[0])}   {item[1]}   {item[2]}')
